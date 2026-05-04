89b7c514c4c152e09d5bd63027c0efec850fcf65
harshitbanaula <harshitbanaula86@gmail.com>
2026-05-04 15:39:33 +0530

    feat: integrate Alembic migrations and remove startup schema creation

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from database.db import create_tables
 from routers import auth
 from routers.email_routers import router as email_routers
 from routers.outreach import router as outreach_router
 from routers.employee_auth import router as employee_router  
-from models.employee import Employee  
 from routers.resume_router import router as resume_router
 from routers.template_router import router as template_router
 from routers.signature_router import router as signature_router
@@ -30,11 +28,6 @@ app.add_middleware(
     allow_methods     = ["*"],
     allow_headers     = ["*"],
 )
-
-@app.on_event("startup")
-def startup():
-    create_tables()
-    print(" Database tables created")
 
 app.include_router(auth.router)
 app.include_router(email_routers)
